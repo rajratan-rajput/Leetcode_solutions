9a8cba6c6cf8837d757b2bddfef0fdb760bdcd54
Rajratan Rajput <rraj30452@gmail.com>
2026-09-08 15:22:49 +0530

    Time: 1 ms (83.65%), Space: 42.6 MB (57.08%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countCommas(int n) {
+        if (n < 1000) {
+            return 0;
+        }
+
+        if (n < 100000) {
+            return n - 999;
+        }
+
+        return 99001;
+    }
+}
